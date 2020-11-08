a42c8f6d0aebc5f7c08326d0cdb4d30c3e02e6d7
Mark Galang <galangmarkg@gmail.com>
2020-11-08 17:49:02 -0500

    Rename file

--- src/main/java/learning/markgalang/spring5WebApp/domain/Book.java
+++ src/main/java/learning/markgalang/spring5WebApp/domain/Book.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-public class Books {
+public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,50 +19,19 @@ public class Books {
             inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> author;
 
-    public Books() {
+    public Book() {
     }
 
-    public Books(String title, String isbn, Set<Author> author) {
-        this.title = title;
-        this.isbn = isbn;
-        this.author = author;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public Book(Long id, String title, String isbn, Set<Author> author) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
         this.isbn = isbn;
-    }
-
-    public Set<Author> getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Set<Author> author) {
         this.author = author;
     }
 
     @Override
     public String toString() {
-        return "Books{" +
+        return "Book{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
@@ -74,8 +43,8 @@ public class Books {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Books books = (Books) o;
-        return Objects.equals(id, books.id);
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
     }
 
     @Override
